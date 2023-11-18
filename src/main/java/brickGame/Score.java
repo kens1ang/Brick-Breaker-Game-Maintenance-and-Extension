c501cd641df6a574f7da394184d8388bbab89fa5
Lai Ken Siang <hfyk13@nottingham.edu.my>
2023-11-19 00:09:01 +0800

    Replaced manual threading with javafx timeline -previous implementation use a new thread for animating each label -javafx timeline give smoother and more reliable animations
    Refactor Score class
    -Define static constants to standardize the positioning of UI elements
    
    Create method 'animateLabel'
    -centralized animation logic using timeline that iterates over keyframe to ensure consistency across different animations
    
    Create method 'createlabel'&'createButton'
    -these methods streamlines the creation of labels and buttons
    -reduce code duplication
    
    Refactor all show method
    -all the show methods are refactored to use the animatelabel, createlabel and createbutton method
    -making it easier to read and understand

--- src/main/java/brickGame/Score.java
+++ src/main/java/brickGame/Score.java
@@ -1,117 +1,93 @@
 package brickGame;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-//import sun.plugin2.message.Message;
+import javafx.util.Duration;
 
 public class Score {
+
+    private static final int LABEL_X = 220;
+    private static final int LABEL_Y = 340;
+    private static final int GAME_OVER_LABEL_X = 200;
+    private static final int GAME_OVER_LABEL_Y = 250;
+    private static final int SCALE_FACTOR = 2;
+    private static final int ANIMATION_STEPS = 21;
+    private static final int ANIMATION_DELAY = 15;
+
     public void show(final double x, final double y, int score, final Main main) {
-        String sign;
-        if (score >= 0) {
-            sign = "+";
-        } else {
-            sign = "";
-        }
+        String sign = score >= 0 ? "+" : "";
         final Label label = new Label(sign + score);
         label.setTranslateX(x);
         label.setTranslateY(y);
-
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 21; i++) {
-                    try {
-                        label.setScaleX(i);
-                        label.setScaleY(i);
-                        label.setOpacity((20 - i) / 20.0);
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        Platform.runLater(() -> main.root.getChildren().add(label));
+        animateLabel(label);
     }
 
     public void showMessage(String message, final Main main) {
         final Label label = new Label(message);
-        label.setTranslateX(220);
-        label.setTranslateY(340);
-
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 21; i++) {
-                    try {
-                        label.setScaleX(Math.abs(i-10));
-                        label.setScaleY(Math.abs(i-10));
-                        label.setOpacity((20 - i) / 20.0);
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        label.setTranslateX(LABEL_X);
+        label.setTranslateY(LABEL_Y);
+        Platform.runLater(() -> main.root.getChildren().add(label));
+        animateLabel(label);
     }
 
+    private void animateLabel(Label label) {
+        int totalAnimationDuration = 1000;
+        int animationSteps = 20;
+
+        Timeline timeline = new Timeline();
+        for (int i = 0; i <= animationSteps; i++) {
+            double fraction = (double) i / animationSteps;
+            double scale = 1 + 0.5 * fraction;
+            double opacity = 1 - fraction;
+
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(i * (totalAnimationDuration / animationSteps)), e -> {
+                label.setScaleX(scale);
+                label.setScaleY(scale);
+                label.setOpacity(opacity);
+            });
+            timeline.getKeyFrames().add(keyFrame);
+        }
+
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
+
+
     public void showGameOver(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("Game Over :(");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
-
-                Button restart = new Button("Restart");
-                restart.setTranslateX(220);
-                restart.setTranslateY(300);
-                restart.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        main.restartGame();
-                    }
-                });
-
-                main.root.getChildren().addAll(label, restart);
-
-            }
+        Platform.runLater(() -> {
+            Label label = createLabel("Game Over :(");
+            Button restart = createButton(event -> main.restartGame());
+            main.root.getChildren().addAll(label, restart);
         });
     }
 
     public void showWin(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("You Win :)");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
-
-
-                main.root.getChildren().addAll(label);
-
-            }
+        Platform.runLater(() -> {
+            Label label = createLabel("You Win :)");
+            main.root.getChildren().add(label);
         });
+    }
+
+    private Label createLabel(String text) {
+        Label label = new Label(text);
+        label.setTranslateX(GAME_OVER_LABEL_X);
+        label.setTranslateY(GAME_OVER_LABEL_Y);
+        label.setScaleX(SCALE_FACTOR);
+        label.setScaleY(SCALE_FACTOR);
+        return label;
+    }
+
+    private Button createButton(EventHandler<ActionEvent> handler) {
+        Button button = new Button("Restart");
+        button.setTranslateX(LABEL_X);
+        button.setTranslateY(370);
+        button.setOnAction(handler);
+        return button;
     }
 }
